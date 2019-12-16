cb53cd8888c1e7f6d1846f4137d418deb8a7b178
jonpas <jonpas33@gmail.com>
2019-12-16 10:44:21 +0100

    Add new apps to INSTALLED_APPS

--- webapps/settings.py
+++ webapps/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo.apps.TodoConfig',
+    'doodle.apps.DoodleConfig',
+    'ludo.apps.LudoConfig',
+    'transport.apps.TransportConfig',
 ]
 
 MIDDLEWARE = [
