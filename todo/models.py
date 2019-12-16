4c46525d6fd4885a51544eaf142eb763659362e8
jonpas <jonpas33@gmail.com>
2019-12-16 21:38:25 +0100

    [todo] Add models and test view with template

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    color = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class List(models.Model):
+    name = models.CharField(max_length=60)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+    completed = models.BooleanField(default=False)
+    deadline = models.DateTimeField(null=True, blank=True)
+    reminder_before_deadline = models.DurationField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.name
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+
+def index(request):
+    lists = models.List.objects.order_by('name')
+    tasks = models.Task.objects.order_by('name')
+
+    context = {
+        'lists': lists,
+        'tasks': tasks,
+    }
+    return render(request, 'todo/index.html', context)
