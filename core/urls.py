c246564a7e672d8d4a2539c1d1437cd9e57b36b6
jonpas <jonpas33@gmail.com>
2019-12-19 15:59:58 +0100

    [all] Add Bootstrap (4.4.1) and style navbar

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,5 @@ app_name = 'core'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/register', views.RegisterView.as_view(), name='register'),
+    path('accounts/register/', views.RegisterView.as_view(), name='register'),
 ]
