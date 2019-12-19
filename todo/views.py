30b38f029564d45cf578e27d9acb3443225331f6
jonpas <jonpas33@gmail.com>
2019-12-19 14:16:21 +0100

    [todo] Add update, create and delete forms for all models

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.urls import reverse_lazy
 
 from . import models
 
@@ -8,6 +9,112 @@ class IndexView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['tags'] = models.Tag.objects.order_by('name')
         context['lists'] = models.List.objects.order_by('name')
         context['tasks'] = models.Task.objects.order_by('name')
+        return context
+
+
+class TagCreateView(generic.CreateView):
+    template_name = 'todo/create.html'
+    model = models.Tag
+    fields = '__all__'
+    success_url = reverse_lazy('todo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'New Tag'
+        return context
+
+
+class TagUpdateView(generic.UpdateView):
+    template_name = 'todo/update.html'
+    model = models.Tag
+    fields = '__all__'
+    success_url = reverse_lazy('todo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edit Tag'
+        return context
+
+
+class TagDeleteView(generic.DeleteView):
+    template_name = 'todo/delete.html'
+    model = models.Tag
+    success_url = reverse_lazy('todo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Delete Tag'
+        return context
+
+
+class ListCreateView(generic.CreateView):
+    template_name = 'todo/create.html'
+    model = models.List
+    fields = '__all__'
+    success_url = reverse_lazy('todo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'New List'
+        return context
+
+
+class ListUpdateView(generic.UpdateView):
+    template_name = 'todo/update.html'
+    model = models.List
+    fields = '__all__'
+    success_url = reverse_lazy('todo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edit List'
+        return context
+
+
+class ListDeleteView(generic.DeleteView):
+    template_name = 'todo/delete.html'
+    model = models.List
+    success_url = reverse_lazy('todo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Delete List'
+        return context
+
+
+class TaskCreateView(generic.CreateView):
+    template_name = 'todo/create.html'
+    model = models.Task
+    fields = '__all__'
+    success_url = reverse_lazy('todo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'New Task'
+        return context
+
+
+class TaskUpdateView(generic.UpdateView):
+    template_name = 'todo/update.html'
+    model = models.Task
+    fields = '__all__'
+    success_url = reverse_lazy('todo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edit Task'
+        return context
+
+
+class TaskDeleteView(generic.DeleteView):
+    template_name = 'todo/delete.html'
+    model = models.Task
+    success_url = reverse_lazy('todo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Delete Task'
         return context
